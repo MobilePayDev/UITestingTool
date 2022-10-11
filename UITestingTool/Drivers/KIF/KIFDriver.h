998b8429af6d428e118f4ccc39eb601b7dddf549
Darius Sabaliauskas <dsab@mobilepay.lt>
2022-10-11 15:10:47 +0300

    Create KIFDriver target

---
+++ UITestingTool/Drivers/KIF/KIFDriver.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for KIFDriver.
+FOUNDATION_EXPORT double KIFDriverVersionNumber;
+
+//! Project version string for KIFDriver.
+FOUNDATION_EXPORT const unsigned char KIFDriverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KIFDriver/PublicHeader.h>
+
+
