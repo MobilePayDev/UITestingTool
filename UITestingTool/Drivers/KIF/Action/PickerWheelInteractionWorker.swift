b8cd9603f230d0a1e894b9f02cc558b44fac7b17
Andrejus Belovas <BELOV@mobilepay.lt>
2022-12-02 09:01:43 +0100

    Merged PR 412096: KIF PickerWheelInteractionWorker implementation
    KIF PickerWheelInteractionWorker implementation

--- UITestingTool/Drivers/KIF/Action/PickerWheelInteractionWorker.swift
+++ UITestingTool/Drivers/KIF/Action/PickerWheelInteractionWorker.swift
@@ -1,8 +1,60 @@
 import UITestingTool
+import UIKit
 
-struct PickerWheelInteractionWorker: InteractionWorker {
+final class PickerWheelInteractionWorker: KIFInteractionWorker {
 
-    init(driver: Driver) {}
+    override func execute(_ interaction: Interaction) {
+        super.execute(interaction)
+        
+        guard let interaction = interaction as? PickerWheelInteraction else {
+            Assert.fail("Invalid interaction", in: interaction.context)
+            return
+        }
+        
+        guard element.view != nil, element.view.isVisibleInWindowFrame() else {
+            Assert.fail("Element does not exist", in: interaction.context)
+            return
+        }
+        
+        if let datePicker = element.view as? UIDatePicker {
+            let date = datePicker.date
+            
+            switch interaction.action {
+            case .index(let index):
+                
+                let period: Int = {
+                    switch interaction.index {
+                    case 0: // days
+                        return 60 * 60 * 24
+                    case 1: // hours
+                        return 60 * 60
+                    case 2: // minutes
+                        return 60 * datePicker.minuteInterval
+                    default:
+                        return 1
+                    }
+                }()
+                
+                let interval = Double(index * period)
+                let modifiedDate = date.addingTimeInterval(interval)
+                datePicker.date = modifiedDate
+                
+            case .value(let value):
+                if interaction.index == 0 {
+                    let monthComponents = Calendar.current.monthSymbols
+                    if let month = monthComponents.firstIndex(of: value), let modifiedDate = Calendar.current.date(bySetting: .month, value: month + 1, of: date) {
+                        datePicker.date = modifiedDate
+                    }
+                } else if interaction.index == 1, let day = Int(value), let modifiedDate = Calendar.current.date(bySetting: .day, value: day + 1, of: date) {
+                    datePicker.date = modifiedDate
+                } else if interaction.index == 2, let year = Int(value), let modifiedDate = Calendar.current.date(bySetting: .year, value: year, of: date) {
+                    datePicker.date = modifiedDate
+                }
+            }
 
-    func execute(_ interaction: Interaction) {}
+            return
+        }
+
+        Assert.fail("Element is not a supported picker", in: interaction.context)
+    }
 }
