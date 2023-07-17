c79a3cbd8113206bd8558e58aad4978d314bd034
Darius Sabaliauskas <sabaliauskas.darius@gmail.com>
2023-07-17 17:04:43 +0300

    Implement child element matching in KIF

--- Drivers/KIFDriver/Element/KIFInteractionElementWorker.swift
+++ Drivers/KIFDriver/Element/KIFInteractionElementWorker.swift
@@ -1,4 +1,5 @@
 import UITestingTool
+import UIKit
 
 class KIFInteractionElementWorker: KIFInteractionWorker, InteractionElementWorker { // swiftlint:disable:this final_class
     func element(for interaction: Interaction) -> Any? {
@@ -8,8 +9,40 @@ class KIFInteractionElementWorker: KIFInteractionWorker, InteractionElementWorke
             atLine: Int(interaction.context.line),
             delegate: self
         )
-        
-        let element = actor?.usingIdentifier(accessibilityInteraction.accessibilityId)
+
+        let element: KIFUIViewTestActor?
+        if let parentElement = (driver as? KIFDriver)?.element {
+            let parentView = parentElement.view
+
+            element = actor?.usingPredicate(NSPredicate(block: { inputView, _ in
+                guard let parentView else { return false }
+                guard let view = inputView as? UIView else { return false }
+                guard view.accessibilityIdentifier == accessibilityInteraction.accessibilityId else { return false }
+
+                let match = view.kif_ancestorMatching { ancestor in
+                    return ancestor == parentView
+                }
+
+                return match != nil
+            }))
+        } else {
+            element = actor?.usingIdentifier(accessibilityInteraction.accessibilityId)
+        }
+
         return element
+    }
+}
+
+fileprivate extension UIView {
+    func kif_ancestorMatching(_ block: (UIView?) -> Bool) -> UIView? {
+        var view = superview
+        while(view != nil) {
+            if block(view) {
+                return view
+            } else {
+                view = view?.superview
+            }
+        }
+        return nil
     }
 }
