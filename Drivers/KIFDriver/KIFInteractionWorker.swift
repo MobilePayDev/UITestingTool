b6b7969a399456c97ce73eb0a586bb834a4ad777
Darius Sabaliauskas <sabaliauskas.darius@gmail.com>
2023-07-19 10:34:26 +0300

    Fix crash on view due to force unwrapping

--- Drivers/KIFDriver/KIFInteractionWorker.swift
+++ Drivers/KIFDriver/KIFInteractionWorker.swift
@@ -4,7 +4,7 @@ import Foundation
 class KIFInteractionWorker: NSObject, InteractionWorker, KIFTestActorDelegateInterface { // swiftlint:disable:this final_class
     let element: KIFUIViewTestActor!
     var accessibilityIdentifier: String {
-        return element.view.accessibilityIdentifier ?? "ACCESSIBILITY ID NOT AVAILABLE"
+        return element.view?.accessibilityIdentifier ?? "ACCESSIBILITY ID NOT AVAILABLE"
     }
     let driver: Driver
 
